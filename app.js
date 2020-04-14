0f04b7d0ebd13891ccd6ae0c5a091714744a3de2
marositibor <m.tibor.csaba@gmail.com>
2020-04-14 20:56:05 +0200

    single blogpost get

--- app.js
+++ app.js
@@ -8,18 +8,26 @@ const app = express()
 const port = 3050
 
 app.use(cors())
+
+app.get('/blog/:id', (req, res) =>{
+    const {id} = req.params
+    db.get("SELECT * from blogpost WHERE id=?",id,(err,row)=>{
+        setTimeout(()=>{res.json(row)},5000)
+    })    
+})
+
 app.get('/blog', (req, res) =>{
-    db.all("SELECT * from blogpost",(err,rows)=>{
+    db.all("SELECT id,author,date,title,description from blogpost",(err,rows)=>{
         setTimeout(()=>{res.json(rows)},5000)
     })
 })
 
 db.serialize(function() {
-    db.run("CREATE TABLE blogpost (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT,date TEXT, title TEXT,description TEXT)");
+    db.run("CREATE TABLE blogpost (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT,date TEXT, title TEXT,description TEXT, content TEXT)");
    
-    db.run("INSERT INTO blogpost (author,date,title,description) VALUES ('Admin','2020/02/03','How to Kungfu','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
-    db.run("INSERT INTO blogpost (author,date,title,description) VALUES ('Admin','2020/02/04','How to Drive','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
-    db.run("INSERT INTO blogpost (author,date,title,description) VALUES ('Admin','2020/02/10','How to Shoot','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO blogpost (author,date,title,content,description) VALUES ('Admin','2020/02/03','How to Kungfu','Lorem ipsum dolor sit amet, consectetur adipiscing elit.','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO blogpost (author,date,title,content,description) VALUES ('Admin','2020/02/04','How to Drive','Lorem ipsum dolor sit amet, consectetur adipiscing elit.','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
+    db.run("INSERT INTO blogpost (author,date,title,content,description) VALUES ('Admin','2020/02/10','How to Shoot','Lorem ipsum dolor sit amet, consectetur adipiscing elit.','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')");
     
   });
 app.listen(port, () => console.log(`Blog backend listening at http://localhost:${port}`))
